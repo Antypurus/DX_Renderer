0ccc2ed6119dd188187323ee14c4bc39ba28671a
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-10-18 14:13:07 +0100

    added debug console

--- DX_Renderer/main.cpp
+++ DX_Renderer/main.cpp
@@ -4,6 +4,12 @@
 int WINAPI CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                    LPSTR lpCmdLine, int nCmdShow)
 {
+	// debug console and shit
+	AllocConsole();
+	freopen("CONIN$", "r", stdin);
+	freopen("CONOUT$", "w", stdout);
+	freopen("CONOUT$", "w", stderr);
+	
 	DXR::Window window{ hInstance,nCmdShow,{1280,720},"DX Renderer" };
 	while(window.ShouldContinue)
 	{
