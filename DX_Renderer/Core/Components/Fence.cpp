c1572e8fd431c8e30a3f0c2e17f817da1af5ce42
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-11-16 17:10:04 +0000

    adding logging

--- DX_Renderer/Core/Components/Fence.cpp
+++ DX_Renderer/Core/Components/Fence.cpp
@@ -8,6 +8,8 @@ namespace DXR
 	{
 		this->initial_value = initialValue;
 		this->current_value = current_value;
+		INFO_LOG(L"Creting Fence\n");
 		DXCall(device->CreateFence(initialValue, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&this->m_fence)));
+		SUCCESS_LOG(L"Fence Created\n");
 	}
 }
