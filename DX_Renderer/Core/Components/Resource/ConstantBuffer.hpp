2e65ca3231bf2a45bd8cf3ae833a56c4bbece6c2
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-12-06 18:52:24 +0000

    moving constant buffer to be a templated type so as to be able to handle any type of data

--- DX_Renderer/Core/Components/Resource/ConstantBuffer.hpp
+++ DX_Renderer/Core/Components/Resource/ConstantBuffer.hpp
@@ -3,16 +3,27 @@
 
 namespace DXR
 {
-
+	template<typename T>
 	struct ConstantBuffer : public Resource
 	{
 	public:
 	private:
+		std::unique_ptr<DescriptorHeap> m_descriptor_heap;
+		std::vector<T> m_data;
 	public:
 	protected:
-		D3D12_RESOURCE_DESC CreateResourceDescription() override;
-		D3D12_CLEAR_VALUE CreateOptimizedClearValue() override;
-		D3D12_HEAP_PROPERTIES CreateResourceHeapDescription() override;
+		D3D12_RESOURCE_DESC CreateResourceDescription() override
+		{
+			D3D12_RESOURCE_DESC
+		}
+
+		D3D12_CLEAR_VALUE CreateOptimizedClearValue() override
+		{
+		}
+
+		D3D12_HEAP_PROPERTIES CreateResourceHeapDescription() override
+		{
+		}
 	};
 
 }
