0aa840377ef33dd4a3a1f2482e14b09d965e3282
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-12-06 01:01:38 +0000

    started working on constant buffer abstraction

---
+++ DX_Renderer/Core/Components/Resource/ConstantBuffer.cpp
@@ -0,0 +1,19 @@
+﻿#include "ConstantBuffer.hpp"
+
+namespace DXR
+{
+	D3D12_RESOURCE_DESC ConstantBuffer::CreateResourceDescription()
+	{
+		D3D12_RESOURCE_DESC resource_description = {};
+
+		return resource_description;
+	}
+
+	D3D12_CLEAR_VALUE ConstantBuffer::CreateOptimizedClearValue()
+	{
+	}
+
+	D3D12_HEAP_PROPERTIES ConstantBuffer::CreateResourceHeapDescription()
+	{
+	}
+}
---
+++ DX_Renderer/Core/Components/Resource/ConstantBuffer.hpp
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "Resource.hpp"
+
+namespace DXR
+{
+
+	struct ConstantBuffer : public Resource
+	{
+	public:
+	private:
+	public:
+	protected:
+		D3D12_RESOURCE_DESC CreateResourceDescription() override;
+		D3D12_CLEAR_VALUE CreateOptimizedClearValue() override;
+		D3D12_HEAP_PROPERTIES CreateResourceHeapDescription() override;
+	};
+
+}
