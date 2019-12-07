f2950332218500a94335218b21e0febcd7316a93
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-12-07 19:56:07 +0000

    working on root signature abstraction

---
+++ DX_Renderer/Core/Components/Shader/Root Signature/ConstantRootParameter.hpp
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "RootParameter.hpp"
+
+namespace DXR
+{
+
+	struct ConstantRootParameter :public RootParameter
+	{
+	private:
+		UINT m_register_slot;
+		UINT m_number_of_values;
+	public:
+		ConstantRootParameter(UINT registerSlot, UINT numberOfValues, D3D12_SHADER_VISIBILITY visibility = D3D12_SHADER_VISIBILITY_ALL);
+	protected:
+		D3D12_ROOT_PARAMETER GenerateRootParameter() override;
+	};
+
+}
