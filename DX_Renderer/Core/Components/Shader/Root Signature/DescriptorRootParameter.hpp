7ec0edf947842ec07b232567a364df8d010488cf
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-12-07 20:23:08 +0000

    working on root signature abstraction

---
+++ DX_Renderer/Core/Components/Shader/Root Signature/DescriptorRootParameter.hpp
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "RootParameter.hpp"
+
+namespace DXR
+{
+
+	enum class DescriptorType
+	{
+		CBV,
+		UAV,
+		SRV
+	};
+	
+	struct DescriptorRootParameter:public RootParameter
+	{
+	public:
+	private:
+		UINT m_register_slot;
+	public:
+		DescriptorRootParameter(DescriptorType Type, UINT registerSlot,D3D12_SHADER_VISIBILITY visibility = D3D12_SHADER_VISIBILITY_ALL);
+	protected:
+		D3D12_ROOT_PARAMETER GenerateRootParameter() override;
+	};
+}
