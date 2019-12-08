4d46edb892f8184b992d17326cccccda345f037a
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-12-08 17:41:19 +0000

    working on depth stencil description abstraction

--- DX_Renderer/Core/Components/Pipeline/DepthStencil.cpp
+++ DX_Renderer/Core/Components/Pipeline/DepthStencil.cpp
@@ -2,5 +2,19 @@
 
 namespace DXR
 {
+	StencilOperation StencilOperation::Default()
+	{
+		return StencilOperation();
+	}
 
+	D3D12_DEPTH_STENCILOP_DESC StencilOperation::GetStencilOperationDescription()
+	{
+		D3D12_DEPTH_STENCILOP_DESC stencil_op = {};
+		stencil_op.StencilDepthFailOp = this->m_stencil_depth_fail_op;
+		stencil_op.StencilFailOp = this->m_stencil_fail_op;
+		stencil_op.StencilFunc = this->m_stencil_function;
+		stencil_op.StencilPassOp = this->m_stencil_pass_op;
+
+		return stencil_op;
+	}
 }
--- DX_Renderer/Core/Components/Pipeline/DepthStencil.hpp
+++ DX_Renderer/Core/Components/Pipeline/DepthStencil.hpp
@@ -1,11 +1,37 @@
 #pragma once
 
+#include <d3d12.h>
+
 namespace DXR
 {
 
+	struct StencilOperation
+	{
+	public:
+	private:
+		D3D12_STENCIL_OP m_stencil_fail_op = D3D12_STENCIL_OP_KEEP;
+		D3D12_STENCIL_OP m_stencil_depth_fail_op = D3D12_STENCIL_OP_KEEP;
+		D3D12_STENCIL_OP m_stencil_pass_op = D3D12_STENCIL_OP_KEEP;
+		D3D12_COMPARISON_FUNC m_stencil_function = D3D12_COMPARISON_FUNC_ALWAYS;
+	public:
+		static StencilOperation Default();
+		D3D12_DEPTH_STENCILOP_DESC GetStencilOperationDescription();
+	private:
+	};
+
 	struct DepthStencil
 	{
-
+	public:
+	private:
+		BOOL m_depth_enabled = TRUE;
+		D3D12_DEPTH_WRITE_MASK m_depth_write_mask = D3D12_DEPTH_WRITE_MASK_ALL;
+		D3D12_COMPARISON_FUNC m_depth_functions = D3D12_COMPARISON_FUNC_LESS;
+		BOOL m_stencil_enabled = FALSE;
+		UINT8 m_stencil_read_mask = D3D12_DEFAULT_STENCIL_READ_MASK;
+		UINT8 m_stencil_write_mask = D3D12_DEFAULT_STENCIL_WRITE_MASK;
+		D3D12_DEPTH_STENCILOP_DESC m_front_face
+	public:
+	private:
 	};
 
 }
