2c6ffbdab1f8e924ccf6f7f3d90aa5c2080b4f11
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-12-08 12:24:11 +0000

    working on pipeline state object

---
+++ DX_Renderer/Core/Components/Pipeline/PipelineStateObject.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <wrl.h>
+#include <d3d12.h>
+
+namespace DXR
+{
+
+	using namespace Microsoft;
+
+	struct PipelineStateObject
+	{
+	public:
+	private:
+	public:
+	private:
+	};
+
+}
