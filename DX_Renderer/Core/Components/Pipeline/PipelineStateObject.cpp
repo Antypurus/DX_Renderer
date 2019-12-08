c7da966909c9e62a05278c9f07a3461b96a82fb2
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-12-08 18:20:46 +0000

    working on pipeline state object abstraction

--- DX_Renderer/Core/Components/Pipeline/PipelineStateObject.cpp
+++ DX_Renderer/Core/Components/Pipeline/PipelineStateObject.cpp
@@ -2,5 +2,27 @@
 
 namespace DXR
 {
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC PipelineStateObject::CreatePipelineStateObjectDescription()
+	{
+		D3D12_GRAPHICS_PIPELINE_STATE_DESC pso = {};
+		pso.BlendState = this->m_blend;
+		pso.CachedPSO = this->m_cached_pso;
+		pso.DepthStencilState = this->m_depth_stencil.GetDepthStencilDescription();
+		pso.DSVFormat = this->m_depth_stencil_buffer_format;
+		pso.Flags = this->m_pso_flags;
+		pso.IBStripCutValue = this->m_index_buffer_strip_cut_value;
+		pso.InputLayout = this->m_input_layout;
+		pso.NodeMask = this->m_node_mask;
+		pso.NumRenderTargets = this->m_render_target_count;
+		pso.PrimitiveTopologyType = this->m_primitive_type;
+		pso.pRootSignature = this->m_root_signature.GetRootSignature();
 
+		pso.VS = this->m_vertex_shader;
+		pso.HS = this->m_hull_shader;
+		pso.DS = this->m_domain_shader;
+		pso.GS = this->m_geometry_shader;
+		pso.PS = this->m_pixel_shader;
+		
+		return pso;
+	}
 }
--- DX_Renderer/Core/Components/Pipeline/PipelineStateObject.hpp
+++ DX_Renderer/Core/Components/Pipeline/PipelineStateObject.hpp
@@ -5,6 +5,7 @@
 #include "Rasterizer.hpp"
 #include "../Shader/Root Signature/RootSignature.hpp"
 #include "Blend.hpp"
+#include "DepthStencil.hpp"
 
 namespace DXR
 {
@@ -14,11 +15,36 @@ namespace DXR
 	{
 	public:
 	private:
-		RootSignature m_root_signature;
-		Rasterizer m_rasterizer = Rasterizer::Default();
+		WRL::ComPtr<ID3D12PipelineState> m_pso;
+
+		// information
+		RootSignature m_root_signature; //passed in
+		D3D12_STREAM_OUTPUT_DESC m_stream_output = {0};
 		Blend m_blend = Blend::Default();
+		Rasterizer m_rasterizer = Rasterizer::Default();
+		DepthStencil m_depth_stencil = DepthStencil::Default();
+		UINT m_sample_mask = UINT_MAX;
+		D3D12_INPUT_LAYOUT_DESC m_input_layout; // passed in
+		D3D12_INDEX_BUFFER_STRIP_CUT_VALUE m_index_buffer_strip_cut_value = D3D12_INDEX_BUFFER_STRIP_CUT_VALUE_DISABLED;
+		D3D12_PRIMITIVE_TOPOLOGY_TYPE m_primitive_type = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+		UINT m_render_target_count = 1;
+		DXGI_FORMAT m_backbuffer_format; // passed in
+		DXGI_FORMAT m_depth_stencil_buffer_format; // passed in
+		DXGI_SAMPLE_DESC m_mssa = {1,0};
+		UINT m_node_mask = 0;
+		D3D12_CACHED_PIPELINE_STATE m_cached_pso = {0};
+		D3D12_PIPELINE_STATE_FLAGS m_pso_flags = D3D12_PIPELINE_STATE_FLAG_NONE;
+
+		// shaders
+		D3D12_SHADER_BYTECODE m_vertex_shader = {0};
+		D3D12_SHADER_BYTECODE m_hull_shader = {0};
+		D3D12_SHADER_BYTECODE m_domain_shader = {0};
+		D3D12_SHADER_BYTECODE m_geometry_shader = {0};
+		D3D12_SHADER_BYTECODE m_pixel_shader = {0};
 	public:
+
 	private:
+		D3D12_GRAPHICS_PIPELINE_STATE_DESC CreatePipelineStateObjectDescription();
 	};
 
 }
