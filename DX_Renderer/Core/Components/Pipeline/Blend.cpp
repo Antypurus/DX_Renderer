8e2169f66f32f678a07d74206f1c445673d18eb9
Tiago Jose Sousa Magalhães <tiagojsmagalhaes@gmail.com>
2019-12-08 14:50:11 +0000

    finished initial version of blend description

--- DX_Renderer/Core/Components/Pipeline/Blend.cpp
+++ DX_Renderer/Core/Components/Pipeline/Blend.cpp
@@ -2,5 +2,48 @@
 
 namespace DXR
 {
+	RenderTargetBlend RenderTargetBlend::Default()
+	{
+		return RenderTargetBlend();
+	}
 
+	D3D12_RENDER_TARGET_BLEND_DESC RenderTargetBlend::GetBlendDescription()
+	{
+		D3D12_RENDER_TARGET_BLEND_DESC render_target_blend = {};
+		render_target_blend.BlendEnable = this->m_blend_enabled;
+		render_target_blend.LogicOpEnable = this->m_logical_operation_enabled;
+
+		render_target_blend.SrcBlend = this->m_source_blend;
+		render_target_blend.DestBlend = this->m_destination_blend;
+		render_target_blend.BlendOp = this->m_blend_operation;
+
+		render_target_blend.SrcBlendAlpha = this->m_source_blend_alpha;
+		render_target_blend.DestBlendAlpha = this->m_destination_blend_alpha;
+		render_target_blend.BlendOpAlpha = this->m_blend_alpha_operation;
+
+		render_target_blend.LogicOp = this->m_logic_operation;
+		render_target_blend.RenderTargetWriteMask = this->m_render_target_write_mask;
+
+		return render_target_blend;
+	}
+
+	Blend Blend::Default()
+	{
+		return Blend();
+	}
+
+	D3D12_BLEND_DESC Blend::GetBlendDescription()
+	{
+		D3D12_BLEND_DESC blend = {};
+		blend.AlphaToCoverageEnable = this->m_alpha_to_coverage_enabled;
+		blend.IndependentBlendEnable = this->m_independent_blend_enabled;
+
+		D3D12_RENDER_TARGET_BLEND_DESC renter_taget_blend = RenderTargetBlend::Default().GetBlendDescription();
+		for(size_t i = 0;i < 8;++i)
+		{
+			blend.RenderTarget[i] = renter_taget_blend;
+		}
+
+		return blend;
+	}
 }
