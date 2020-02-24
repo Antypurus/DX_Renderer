7f69bd2546896bcb48c75316acba2720e64d49d7
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-02-24 05:37:16 +0000

    started working on render target view abstraction

---
+++ DX_Renderer/Core/Components/Resource/RenderTargetView.cpp
@@ -0,0 +1,10 @@
+﻿#include "RenderTargetView.h"
+
+namespace DXR
+{
+	RenderTargetView::RenderTargetView(ID3D12Resource* RenderTarget, DescriptorHeap& DescriptorHeap,
+		Resolution& Resolution):Resource(DescriptorHeap)
+	{
+		this->m_resource = WRL::ComPtr<ID3D12Resource>(RenderTarget);
+	}
+}
---
+++ DX_Renderer/Core/Components/Resource/RenderTargetView.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "Resource.hpp"
+
+namespace DXR
+{
+	struct Resolution;
+
+	class RenderTargetView : public DXR::Resource
+	{
+	public:
+	private:
+	public:
+		RenderTargetView(ID3D12Resource* RenderTarget, DescriptorHeap& DescriptorHeap, Resolution& Resolution);
+	private:
+	};
+}
