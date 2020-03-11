d1a949cc783a53a716b553a146285e1d24f7a2ac
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-03-11 19:11:30 +0000

    added missing files

---
+++ DX_Renderer/Core/Components/Resource/RenderTargetView.cpp
@@ -0,0 +1,19 @@
+﻿#include "RenderTargetView.hpp"
+#include "../Command List/GraphicsCommandList.hpp"
+#include "DescriptorHeap.hpp"
+
+namespace DXR
+{
+	RenderTargetView::RenderTargetView(WRL::ComPtr<ID3D12Resource>& RenderTarget, DescriptorHeap& DescriptorHeap,
+		Resolution& Resolution, size_t HeapIndex):Resource(DescriptorHeap, HeapIndex)
+	{
+		RenderTarget.CopyTo(IID_PPV_ARGS(&this->m_resource));
+		this->m_descriptor_heap = &DescriptorHeap;
+		this->m_heap_index = HeapIndex;
+	}
+
+	void RenderTargetView::Clear(GraphicsCommandList& CommandList, FLOAT ClearColor[4]) const
+	{
+		CommandList->ClearRenderTargetView((*this->m_descriptor_heap)[this->m_heap_index], ClearColor, 0, nullptr);
+	}
+}
