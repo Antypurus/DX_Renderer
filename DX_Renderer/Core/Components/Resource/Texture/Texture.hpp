fe1d8ca19b5e06f2568f753a3229a8b7e653d982
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-03-24 04:15:01 +0000

    removed unnecessary code

--- DX_Renderer/Core/Components/Resource/Texture/Texture.hpp
+++ DX_Renderer/Core/Components/Resource/Texture/Texture.hpp
@@ -12,8 +12,8 @@ namespace  DXR
 	private:
 		TextureData m_texture_data;
 		DXGI_FORMAT m_texture_format;
-		//std::unique_ptr<GPUUploadBuffer> m_upload_buffer;
-		//std::unique_ptr<GPUDefaultBuffer> m_texture_buffer;
+		std::unique_ptr<GPUUploadBuffer> m_upload_buffer = nullptr;
+		std::unique_ptr<GPUDefaultBuffer> m_texture_buffer = nullptr;
 	public:
 		Texture(const std::wstring& filepath);
 	private:
