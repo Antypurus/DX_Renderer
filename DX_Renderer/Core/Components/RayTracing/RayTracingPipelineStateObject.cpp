2da03cb310cbb6910508a90e8ab4e0adc153c263
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-03-31 11:44:30 +0100

    rtpso should now be created

--- DX_Renderer/Core/Components/RayTracing/RayTracingPipelineStateObject.cpp
+++ DX_Renderer/Core/Components/RayTracing/RayTracingPipelineStateObject.cpp
@@ -1,4 +1,5 @@
 #include "RayTracingPipelineStateObject.hpp"
+#include "../GraphicsDevice.hpp"
 
 namespace DXR
 {
@@ -14,6 +15,26 @@ namespace DXR
 		ShaderExports[0] = this->m_ray_gen_shader->GetUniqueID().c_str();
 		ShaderExports[1] = this->m_miss_shader->GetUniqueID().c_str();
 		ShaderExports[2] = L"HitGroup";
+
+		D3D12_STATE_SUBOBJECT subobjects[11];
+		subobjects[0] = this->m_ray_gen_shader->GenerateShaderExport();
+		subobjects[1] = this->m_intersection_shader->GenerateShaderExport();
+		subobjects[2] = this->m_any_hit_shader->GenerateShaderExport();
+		subobjects[3] = this->m_closest_hit_shader->GenerateShaderExport();
+		subobjects[4] = this->m_miss_shader->GenerateShaderExport();
+		subobjects[5] = this->shader_config;
+		subobjects[6] = this->hit_group;
+		subobjects[7] = this->export_association;
+		subobjects[8] = this->pipeline_config;
+		subobjects[9] = this->root_signature;
+		subobjects[10] = this->rs_association;
+
+		D3D12_STATE_OBJECT_DESC rtpso_desc = {};
+		rtpso_desc.Type = D3D12_STATE_OBJECT_TYPE_RAYTRACING_PIPELINE;
+		rtpso_desc.NumSubobjects = 11;
+		rtpso_desc.pSubobjects = subobjects;
+
+		Device->CreateStateObject(&rtpso_desc,IID_PPV_ARGS(&this->m_rtpso));
 	}
 
 	D3D12_STATE_SUBOBJECT RayTracingPipelineStateObject::CreateShaderConfiguration()
