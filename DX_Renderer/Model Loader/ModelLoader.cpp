c2d4173a483c25589b6f1f112d5be9496d5799bc
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-04-12 02:11:58 +0100

    working on model loading

--- DX_Renderer/Model Loader/ModelLoader.cpp
+++ DX_Renderer/Model Loader/ModelLoader.cpp
@@ -2,5 +2,8 @@
 
 namespace DXR
 {
-
+	OBJModel OBJModelLoader::Load(const std::wstring& filepath, GraphicsDevice& Device, GraphicsCommandList& CommandList)
+	{
+		return OBJModel();
+	}
 }
--- DX_Renderer/Model Loader/ModelLoader.hpp
+++ DX_Renderer/Model Loader/ModelLoader.hpp
@@ -1,17 +1,25 @@
 #pragma once
 #include "../ThirdParty/tiny_obj_loader/tiny_obj_loader.h"
+#include "ModelVertex.hpp"
+#include <string>
 
 namespace DXR
 {
 
+	struct GraphicsDevice;
+	struct GraphicsCommandList;
+
 	struct OBJModel
 	{
 	public:
+		std::vector<OBJVertex> vertices;
+		std::vector<UINT> indices;
 	};
 
 	struct OBJModelLoader
 	{
 	public:
+		static OBJModel Load(const std::wstring& filepath, GraphicsDevice& Device, GraphicsCommandList& CommandList);
 	};
 
 }
