515a1dd5dd5e6c707c25b696384cbe245399fae1
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-05-03 02:15:10 +0100

    started work on the refactor on the voxelizer

--- DX_Renderer/Voxel/Voxelizer.hpp
+++ DX_Renderer/Voxel/Voxelizer.hpp
@@ -2,6 +2,8 @@
 
 #include "../Core/Components/Shader/PixelShader.hpp"
 #include "../Core/Components/Shader/VertexShader.hpp"
+#include "../Core/Components/Pipeline/PipelineStateObject.hpp"
+#include "../Core/Components/RayTracing/AccelerationStructure.hpp"
 #include "VoxelMap.hpp"
 
 #include <directxmath.h>
@@ -19,10 +21,13 @@ namespace DXR
         PixelShader voxelization_pixel_shader;
         VoxelMap voxel_map;
         Model* model;
-        //NOTE(Tiago):voxelization matrices support data
+        PipelineStateObject pso;
+        TLAS acceleration_structure;
+        //NOTE(Tiago):Voxelization matrices support data
         XMFLOAT3 extent;
         XMFLOAT3 center;
         XMFLOAT3 voxel_space;
+        //NOTE(Tiago):Voxelization matrices
         XMMATRIX voxel_space_conversion_matrix;
         XMMATRIX clip_space_conversion_matrix;
         
