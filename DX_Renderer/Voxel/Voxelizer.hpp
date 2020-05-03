3c766a2975658dfcb97ec8cfb386217f574ec216
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-05-03 04:17:47 +0100

    started work on the refactor on the voxelizer

--- DX_Renderer/Voxel/Voxelizer.hpp
+++ DX_Renderer/Voxel/Voxelizer.hpp
@@ -4,18 +4,27 @@
 #include "../Core/Components/Shader/VertexShader.hpp"
 #include "../Core/Components/Pipeline/PipelineStateObject.hpp"
 #include "../Core/Components/RayTracing/AccelerationStructure.hpp"
+#include "../Core/Components/Resource/GPU Buffers/ConstantBuffer.hpp"
 #include "VoxelMap.hpp"
 
 #include <directxmath.h>
+#include <memory>
 
 namespace DXR
 {
     
+    struct Camera;
     struct GraphicsDevice;
     struct GraphicsCommandList;
     struct Model;
     struct Swapchain;
     struct RootSignature;//TODO(Tiago): Build root signature specifically for voxelization?
+    
+    struct Voxelization_CBuffer
+    {
+        XMMATRIX clip_space_transformation_matrix;
+        XMMATRIX voxel_space_transformation_matrix;
+    };
     
     struct Voxelizer
     {
@@ -25,6 +34,7 @@ namespace DXR
         Model* model;
         PipelineStateObject pso;
         TLAS acceleration_structure;
+        std::unique_ptr<ConstantBuffer<Voxelization_CBuffer>> voxelization_cbuffer;
         //NOTE(Tiago):Voxelization matrices support data
         XMFLOAT3 extent;
         XMFLOAT3 center;
@@ -40,6 +50,10 @@ namespace DXR
                   RootSignature& root_signature,
                   Model& model,
                   XMMATRIX mvp);
+        private:
+        void CalculateVoxelizationSupportData();
+        void CreateVoxelizationMatrices(Camera& camera, XMMATRIX& model_matrix);
+        void UpdateVoxelizationCBuffer();
     };
     
 }
