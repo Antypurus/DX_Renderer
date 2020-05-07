f91dce514285484a19a4a4279c444cc36e22f252
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-05-07 20:04:28 +0100

    trying to fix voxelization

--- DX_Renderer/Voxel/Voxelizer.cpp
+++ DX_Renderer/Voxel/Voxelizer.cpp
@@ -8,12 +8,12 @@
 
 namespace DXR
 {
-
+    
 	Voxelizer::Voxelizer(GraphicsDevice& device,
-		GraphicsCommandList& command_list,
-		RootSignature& root_signature,
-		Model& model,
-		XMMATRIX mvp)
+                         GraphicsCommandList& command_list,
+                         RootSignature& root_signature,
+                         Model& model,
+                         XMMATRIX mvp)
 	{
 		this->CreateVoxelCube(device, command_list);
 		this->model_vertex_buffer = model.GenerateVertexBuffer(device, command_list);
@@ -22,19 +22,19 @@ namespace DXR
 		this->voxel_map = VoxelMap(device, VOXEL_WIDTH, VOXEL_HEIGHT, VOXEL_DEPTH);
 		this->model = &model;
 		this->pso = PipelineStateObject(device,
-			this->voxelization_vertex_shader.GetShaderBytecode(),
-			this->voxelization_pixel_shader.GetShaderBytecode(),
-			root_signature,
-			OBJVertex::GetInputLayout(),
-			Swapchain::m_backbuffer_format,
-			DepthStencilBuffer::DepthStencilBufferFormat);
+                                        this->voxelization_vertex_shader.GetShaderBytecode(),
+                                        this->voxelization_pixel_shader.GetShaderBytecode(),
+                                        root_signature,
+                                        OBJVertex::GetInputLayout(),
+                                        Swapchain::m_backbuffer_format,
+                                        DepthStencilBuffer::DepthStencilBufferFormat);
 		//NOTE(Tiago): dummy/placeholder data is required to create the constant buffer while the matrices are not yet computed
 		std::vector<Voxelization_CBuffer> intermediate;
 		intermediate.push_back({ XMMatrixIdentity(), XMMatrixIdentity() });
 		this->voxelization_cbuffer = std::make_unique<ConstantBuffer<Voxelization_CBuffer>>(device, intermediate);
 		this->CalculateVoxelizationSupportData();
 	}
-
+    
 	void Voxelizer::Voxelize(GraphicsCommandList& command_list, Camera& camera, RootSignature& root_signature, XMMATRIX model_matrix, UINT constant_buffer_slot, UINT voxel_map_uav_slot)
 	{
 		voxel_map.Clear(command_list);
@@ -49,11 +49,11 @@ namespace DXR
 		command_list.BindIndexBuffer(model_index_buffer);
 		command_list.SendDrawCall();
 	}
-
+    
 	void Voxelizer::CalculateVoxelizationSupportData()
 	{
 		using namespace std;
-
+        
 		XMFLOAT3* AABB = model->AABB;
 		XMVECTOR extent_vector = XMVectorSubtract({ AABB[1].x,AABB[1].y,AABB[1].z,1 }, { AABB[0].x,AABB[0].y,AABB[0].z,1 });
 		XMStoreFloat3(&extent, extent_vector);
@@ -61,16 +61,16 @@ namespace DXR
 		extent.y *= float(VOXEL_HEIGHT + 2.0f) / float(VOXEL_HEIGHT);
 		extent.z *= float(VOXEL_DEPTH + 2.0f) / float(VOXEL_DEPTH);
 		//extent.x = extent.y = extent.z = max(extent.x, max(extent.y, extent.z));
-
+        
 		XMVECTOR center_vector = XMVectorAdd({ AABB[1].x,AABB[1].y,AABB[1].z,1 }, { AABB[0].x,AABB[0].y,AABB[0].z,1 });
 		center_vector = XMVectorScale(center_vector, 0.5f);
 		XMStoreFloat3(&center, center_vector);
-
+        
 		XMVECTOR voxel_space_vector = XMVectorScale({ extent.x,extent.y,extent.z,1 }, 0.5f);
 		voxel_space_vector = XMVectorSubtract(center_vector, voxel_space_vector);
 		XMStoreFloat3(&voxel_space, voxel_space_vector);
 	}
-
+    
 	void Voxelizer::UpdateVoxelizationMatrices(Camera& camera, XMMATRIX& model_matrix)
 	{
 		XMMATRIX voxel_matrix;
@@ -78,15 +78,15 @@ namespace DXR
 		m1 = XMMatrixTranslation(-voxel_space.x, -voxel_space.y, -voxel_space.z);
 		m2 = XMMatrixScaling(VOXEL_WIDTH / extent.x, VOXEL_HEIGHT / extent.y, VOXEL_DEPTH / extent.z);
 		voxel_matrix = m1 * m2;
-		voxel_space_conversion_matrix = model_matrix * camera.ViewMatrix() * voxel_matrix;
-
+		voxel_space_conversion_matrix = model_matrix * voxel_matrix;
+        
 		XMMATRIX clip_matrix;
 		m1 = XMMatrixTranslation(-center.x, -center.y, -voxel_space.z);
 		m2 = XMMatrixScaling(2.0f / extent.x, 2.0f / extent.y, 1.0f / extent.z);
 		clip_matrix = m1 * m2;
-		clip_space_conversion_matrix = model_matrix * camera.ViewMatrix() * clip_matrix;
+		clip_space_conversion_matrix = model_matrix * clip_matrix;
 	}
-
+    
 	void Voxelizer::UpdateVoxelizationCBuffer()
 	{
 		voxelization_cbuffer->UpdateData({ {
@@ -94,7 +94,7 @@ namespace DXR
 												 voxel_space_conversion_matrix
 											 } });
 	}
-
+    
 	void Voxelizer::CreateVoxelizationShaders()
 	{
 		const std::wstring voxel_shader_path = L"./DX_Renderer/Resources/Shaders/Voxelization.hlsl";
@@ -103,8 +103,8 @@ namespace DXR
 		this->voxelization_vertex_shader = VertexShader::CompileShaderFromFile(voxel_shader_path, VS_entrypoint);
 		this->voxelization_pixel_shader = PixelShader::CompileShaderFromFile(voxel_shader_path, PS_entrypoint);
 	}
-
-
+    
+    
 	void Voxelizer::SetViewport(GraphicsCommandList& command_list)
 	{
 		D3D12_VIEWPORT viewport = {};
@@ -116,7 +116,7 @@ namespace DXR
 		viewport.TopLeftY = 0;
 		command_list->RSSetViewports(1, &viewport);
 	}
-
+    
 	void Voxelizer::CreateVoxelCube(GraphicsDevice& device, GraphicsCommandList& command_list)
 	{
 		std::vector<Vertex> vertices = {
@@ -147,7 +147,7 @@ namespace DXR
 		voxel_cube_index_buffer = std::make_unique<IndexBuffer>(device, command_list, indices);
 		voxel_cube_blas = std::make_unique<BLAS>(device, command_list, *voxel_cube_vertex_buffer, *voxel_cube_index_buffer);
 	}
-
+    
 	void Voxelizer::BuildAccelerationStructure(GraphicsDevice& device, GraphicsCommandList& command_list, Fence& fence)
 	{
 		if (acceleration_structure.GetInstanceCount() == 0)
@@ -173,5 +173,5 @@ namespace DXR
 			}
 		}
 	}
-
+    
 }
