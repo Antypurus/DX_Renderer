c56f4c0f8490c0ebd4bec489014b816434d1a492
Tiago Magalhaes <tiagojsmagalhaes@gmail.com>
2020-05-17 20:39:45 +0100

    still trying to hide voxel grid

--- DX_Renderer/Resources/Shaders/VertexShader.hlsl
+++ DX_Renderer/Resources/Shaders/VertexShader.hlsl
@@ -70,8 +70,8 @@ PS_OUTPUT PSMain(VS_OUTPUT input)
                           (input.u_pos.y-input.light_pos.y)*(input.u_pos.y-input.light_pos.y)+
                           (input.u_pos.z-input.light_pos.z)*(input.u_pos.z-input.light_pos.z));
     float falloff = 1/abs(distance+0.1);
-    //float4 other_col = irradiance_map_tex.Sample(gsampler, float3(vox.x / 64.0f,vox.y / 64.0f,vox.z / 768.0f));
-    float4 other_col = irradiance_map[voxel];
+    float4 other_col = irradiance_map_tex.Sample(gsampler, float3(vox.x / 256.0f,vox.y / 256.0f,vox.z / 768.0f));
+    //float4 other_col = irradiance_map[voxel];
     
     output.color = 0.25 * col + 0.75 * falloff * other_col * col;
 	return output;
